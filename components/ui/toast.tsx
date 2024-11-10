6692fb3e70f629b60cf08105da406d850f3d51e2
mridul-iiitn <mridulpandey619@gmail.com>
2024-11-10 17:38:13 +0530

    Update in ui/toast.tsx

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -4,7 +4,6 @@ import * as React from "react"
 import * as ToastPrimitives from "@radix-ui/react-toast"
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
-
 import { cn } from "@/lib/utils"
 
 const ToastProvider = ToastPrimitives.Provider
@@ -113,7 +112,6 @@ const ToastDescription = React.forwardRef<
 ToastDescription.displayName = ToastPrimitives.Description.displayName
 
 type ToastProps = React.ComponentPropsWithoutRef<typeof Toast>
-
 type ToastActionElement = React.ReactElement<typeof ToastAction>
 
 export {
